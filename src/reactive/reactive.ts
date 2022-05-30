7ea8cff277acbd55145020d1e031b089e5e3e97b
王建魁(wangjiankui) <wangjiankui@longfor.com>
2022-05-30 12:42:03 +0800

    fix: 修改名称错误 toRow => toRaw

--- src/reactive/reactive.ts
+++ src/reactive/reactive.ts
@@ -108,7 +108,7 @@ export function isReactive(value: unknown): boolean {
 }
 
 // 返回响应式对象的原始数据
-export function toRow<T>(observed: T): T {
+export function toRaw<T>(observed: T): T {
   const raw = observed && (observed as  Target)[ReactiveFlags.RAW]
-  return raw ? toRow(raw) : observed
+  return raw ? toRaw(raw) : observed
 }
