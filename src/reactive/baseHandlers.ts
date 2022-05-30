71a6f6bf657406933e5770ecce8f506256770daf
王建魁(wangjiankui) <wangjiankui@longfor.com>
2022-05-30 12:00:10 +0800

    feat: 实现reactive 嵌套ref 情况
    eg: reactive({ foo: ref('foo') })

--- src/reactive/baseHandlers.ts
+++ src/reactive/baseHandlers.ts
@@ -1,21 +1,36 @@
 import {reactive, ReactiveFlags, Target} from "./reactive";
 import { track, trigger } from "./effect";
 import { isObject } from "../shared";
+import {isRef} from "./ref";
 
-function createGetter() {
+function createGetter(isReadonly = false, shallow = false) {
   return function get (target: Target, key: string | symbol, receiver: any): any {
     /*
     * 处理像这种情况的访问 target['__v_isReactive']
     * 当判断是不是一个reactive是有用，例如 isReactive() 函数
     * */
-    if (key === ReactiveFlags.IS_REACTIVE) {
-      return true
+    if (key === ReactiveFlags.IS_REACTIVE) { // '__v_isReactive'
+      return !isReadonly
+    } else if (key === ReactiveFlags.IS_READONLY) { // '__v_isReadonly'
+      return isReadonly
     }
+
     const res = Reflect.get(target, key, receiver)
+
     track(target, key)
+
+    /*
+    * 处理嵌套的ref(), 也叫做解包ref
+    * 例如 reactive({ foo: ref('foo')})
+    * */
+    if (isRef(res)) {
+      return res.value
+    }
+
     if (isObject(res)) {
       return reactive(res)
     }
+
 
     return res
   }
