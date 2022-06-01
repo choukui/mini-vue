3df3b19d49210e7a2fd1564556eaf5301fc87988
王建魁(wangjiankui) <wangjiankui@longfor.com>
2022-06-01 08:35:13 +0800

    feat: 实现isProxy方法

--- src/reactive/reactive.ts
+++ src/reactive/reactive.ts
@@ -112,6 +112,11 @@ export function isReactive(value: unknown): boolean {
   return !!(value && (value as Target)[ReactiveFlags.IS_REACTIVE])
 }
 
+// 是否是个响应式对象
+export function isProxy(value: unknown): boolean {
+  return isReactive(value) || isReadonly(value)
+}
+
 // 返回响应式对象的原始数据
 export function toRaw<T>(observed: T): T {
   const raw = observed && (observed as  Target)[ReactiveFlags.RAW]
