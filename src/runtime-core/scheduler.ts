f3f34da5b3b844ca65e849a7efa9337e30bd20bf
王建魁(wangjiankui) <wangjiankui@longfor.com>
2022-06-14 11:05:28 +0800

    feat(nextTick): 实现nextTick函数

--- src/runtime-core/scheduler.ts
+++ src/runtime-core/scheduler.ts
@@ -1,9 +1,19 @@
 import { ComponentInternalInstance } from "./component";
-
+/********** TS类型声明 start ***********/
 export interface SchedulerJob extends Function {
   id?: number
   active?: boolean
   computed?: boolean
   allowRecurse?: boolean
   ownerInstance?: ComponentInternalInstance
+}
+/********** TS类型声明 end ***********/
+const resolvedPromise: Promise<any> = Promise.resolve()
+let currentFlushPromise: Promise<void> | null = null
+export function nextTick<T = void>(
+  this: T,
+  fn?: (this: T) => void
+): Promise<void> {
+  const p = currentFlushPromise || resolvedPromise
+  return fn ? p.then(this ? fn.bind(this) : fn) : p
 }
