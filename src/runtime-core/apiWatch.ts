0a597c56b2d35439a757bd64196cf177d39b74c8
王建魁(wangjiankui) <wangjiankui@longfor.com>
2022-07-16 10:01:59 +0800

    refactor(watch): 增加注释

--- src/runtime-core/apiWatch.ts
+++ src/runtime-core/apiWatch.ts
@@ -93,14 +93,17 @@ export function watch<T = any, Immediate extends Readonly<boolean> = false>(
 ): WatchStopHandle {
   return doWatch(source as any, cb, options)
 }
-
+/*
+* 通常watch函数接收两个参数
+* source: 响应式数据
+* cb: 回调函数*/
 function doWatch(
   source: WatchSource | WatchSource[] | WatchEffect | object,
   cb: WatchCallback | null,
   { immediate, deep, flush, onTrack, onTrigger }: WatchOptions = EMPTY_OBJ
 ): WatchStopHandle {
 
-  let getter: () => any
+  let getter: () => any // 定义getter
   let forceTrigger = false // flag 强制触发
   let isMultiSource = false // flag, source = [ value, value2 ]这种情况
   if (isRef(source)) { // ref
@@ -118,13 +121,13 @@ function doWatch(
       if (isRef(s)) {
         return s.value
       } else if (isReactive(s)) {
-        // 递归返回值
+        // 调用traverse递归读取
         return traverse(s)
       } else if (isFunction(s)) {
         return s()
       }
     })
-  } else if (isFunction(source)) { // function
+  } else if (isFunction(source)) { // 如果 source 是函数，说明用户传入的是 getter, 直接把 source 赋值给 getter
     if (cb) { // watch函数
       getter = () => source(onInvalidate)
     } else { // 没有callback就是一个watchEffect
@@ -157,13 +160,13 @@ function doWatch(
     }
   }
 
-  let oldValue = isMultiSource ? [] : {}
+  let oldValue = isMultiSource ? [] : {} // 储存旧值
   const job: SchedulerJob = () => {
     if (!effect.active) { // effect stop 不用执行
       return
     }
-    if (cb) { // watch(source, callback)
-      const newValue = effect.run()
+    if (cb) { // eg: watch(source, callback)
+      const newValue = effect.run() // 在 scheduler 中重新执行副作用函数，获取新值
       // 自己提出一个函数，源码里太长了不好阅读，功能一样
       const _hasChange = () => {
         return isMultiSource
@@ -172,12 +175,13 @@ function doWatch(
       }
       // 新旧值有变化才触发回调
       if (deep || forceTrigger || _hasChange()) {
-        //在运行之前 先清除一下副作用
+        //在调用回调函数cb之前 先调用过期回调cleanup
         if (cleanup) {
           cleanup()
         }
-        // 给回调函数传入新旧两个值，
+        // 给回调函数传入新旧两个值，并将 onInvalidate 作为回调函数的第三个参数，以便用户调用
         cb(newValue, oldValue, onInvalidate)
+        // 更新旧值，不然下次会得到错误的旧值
         oldValue = newValue
       }
     } else { // watchEffect(effect) 直接run
@@ -185,7 +189,7 @@ function doWatch(
     }
   }
 
-  // 调度器 pre
+  // 当数据变化时执行调度器(pre类型)
   let scheduler: EffectScheduler = () => {
     // 源码里是放在队列里执行的，这里简单放在微任务里。
     // 目的防止同一个宏任务多个状态改变，多次触发回调函数执行
@@ -201,7 +205,7 @@ function doWatch(
     if (immediate) { // immediate 立即执行
       job()
     } else {
-      // 先主动获取下，拿到旧值
+      // 手动调用下副作用函数，获取旧值
       oldValue = effect.run()
     }
   } else { // watchEffect
@@ -214,17 +218,19 @@ function doWatch(
   }
 }
 
-// 这块直接从源码复制过来的，没看懂这块的作用,
-// todo 待深入研究这块的作用
+// 递归读取对象的每一个属性
 // seen 是用于防止递归陷入死循环
 export function traverse(value: unknown, seen?: Set<unknown>) {
+  // 如果是读取的原始值，什么都不做
   if (!isObject(value) || (value as any)[ReactiveFlags.SKIP]) {
     return value
   }
   seen = seen || new Set()
+  // 如果已经被读取过了，同样什么也不做
   if (seen.has(value)) {
     return value
   }
+  // 将数据添加到seen中，代表遍历读取过了。避免循环引用引起死循环
   seen.add(value)
   if (isRef(value)) {
     traverse(value.value, seen)
@@ -237,6 +243,7 @@ export function traverse(value: unknown, seen?: Set<unknown>) {
       traverse(v, seen)
     })
   } else if (isPlainObject(value)) {
+    // 如果是一个普通对象，使用for in 读取对象的每一个值，并递归地调用traverse进行处理
     for (const key in value) {
       traverse((value as any)[key], seen)
     }
